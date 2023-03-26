7cbc609d90c5ddebe0448069ae6e3e81d7dfc0b1
tomere13 <98044440+tomere13@users.noreply.github.com>
2023-03-26 20:30:27 +0300

    f

---
+++ src/App.js
@@ -0,0 +1,64 @@
+import React, { useState } from 'react'
+import SingleColor from './SingleColor'
+
+import Values from 'values.js'
+
+function App() {
+  const [color, setColor] = useState('#f15025')
+  const [error, setError] = useState(false)
+  const [list, setList] = useState([])
+  var colors = new Values(color).all(10)
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    try {
+      setError(false)
+
+      setList(colors)
+    } catch (error) {
+      setError(true)
+      console.log(error)
+    }
+  }
+  return (
+    <>
+      <section className="container">
+        <h3>color generator</h3>
+        <form className="color-form" method="submit" onSubmit={handleSubmit}>
+          <input
+            type="color"
+            value={color}
+            style={{
+              appearance: 'auto',
+              width: '50px',
+              height: '42px',
+              cursor: 'default',
+
+              backgroundColor: color,
+            }}
+            onChange={(e) => setColor(e.target.value)}
+          />
+          <input
+            type={'text'}
+            placeholder="#f15025"
+            value={color}
+            onChange={(e) => setColor(e.target.value)}
+            className={`${error ? 'error' : null}`}
+          />
+          <button
+            className="btn"
+            style={{ backgroundColor: color }}
+            method="submit">
+            Generate
+          </button>
+        </form>
+      </section>
+      <section className="colors">
+        {list.map((color, index) => {
+          return <SingleColor key={index} {...color} index={index} />
+        })}
+      </section>
+    </>
+  )
+}
+
+export default App
---
+++ src/SingleColor.js
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from 'react'
+import rgbToHex from './utils'
+
+const SingleColor = ({ rgb, weight, index }) => {
+  const [alert, setAlert] = useState(false)
+  const bcg = rgb.join(',')
+  const hexColor = rgbToHex(...rgb)
+  return (
+    <article
+      onClick={() => {
+        setAlert(true)
+        navigator.clipboard.writeText(hexColor)
+      }}
+      className={`color ${index >= 10 && 'color-light'}`}
+      style={{
+        backgroundColor: `rgb(${bcg})`,
+      }}>
+      <p className="percent-value">{weight}%</p>
+      <p className="color-value">{hexColor}</p>
+      <p style={{ fontSize: '18px' }}>
+        {alert ? 'Coppied to the clipboard' : 'Press me to copy the color'}
+      </p>
+    </article>
+  )
+}
+
+export default SingleColor
